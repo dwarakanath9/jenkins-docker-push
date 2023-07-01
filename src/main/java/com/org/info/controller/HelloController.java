a7f416edcc484213806977e25b412361871b395a
dwarakanath9 <dwarakanathc9@gmail.com>
2023-07-01 16:04:05 +0100

    adding code for reading computer name

--- src/main/java/com/org/info/controller/HelloController.java
+++ src/main/java/com/org/info/controller/HelloController.java
@@ -1,18 +1,27 @@
 package com.org.info.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 public class HelloController {
 
     @GetMapping
-    public String sayHello(){
-        return "Hellow i am from docker";
+    public String sayHello() throws UnknownHostException{
+        return " your name is :" + InetAddress.getLocalHost().getHostName();
     }
 
     @GetMapping("/hello")
     public String sayHelloDocker(){
         return "Hellow i am from docker hellow 1";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHelloDocker(String name)  {
+        return " your name is :" + name;
     }
 }
