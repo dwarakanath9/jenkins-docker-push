b775b85dcb8aaf05f078275f354dbc306e04c4f9
dwarakanath9 <dwarakanathc9@gmail.com>
2023-07-01 15:11:22 +0100

    first-commit

---
+++ src/main/java/com/org/info/JenkinsDockerPushApplication.java
@@ -0,0 +1,13 @@
+package com.org.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerPushApplication.class, args);
+	}
+
+}
